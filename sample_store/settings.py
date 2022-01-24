cb5602fb5e202042e931945a3fd0e706e2f57faf
Callymags <callanmaguire18@gmail.com>
2022-01-24 11:42:10 +0000

    deployment: Change secret key and debug settings

--- sample_store/settings.py
+++ sample_store/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m*hz!a029!&2&i9*hk@$rm@at1&b_j4za79ng)(-*@%y5j1)mv'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['callymags-sample-store.herokuapp.com', 'localhost']
 
